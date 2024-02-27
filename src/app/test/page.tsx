428bf07ad6bd4621f92e8dd46a8a7f316ccfaadd
diptanshumahish <diptanshu@fyipen.com>
2024-02-27 12:08:00 +0530

    new test video

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,7 +4,7 @@ import { static_images } from "@/static/static_images";
 import React from "react";
 
 const source4 =
-    "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8";
+    "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8";
 export default function page() {
     return (
         <div>
